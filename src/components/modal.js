dceb6dc2e57ee8512511806f0846743fa0aabceb
koroed010 <koroed010@yandex.ru>
2024-08-10 20:26:06 +0300

    to the 3-rd review

--- src/components/modal.js
+++ src/components/modal.js
@@ -2,17 +2,18 @@
 // функция открытия окна
 export function openModal(modal) {
     modal.classList.add('popup_is-opened');
-    document.addEventListener('keydown', (evt) => {
-        if (evt.key === 'Escape') { 
-            closeModal(modal);
-        };
-    });
+    document.addEventListener('keydown', closeByEscape);
 };
 
 // функция закрытия окна 
 export function closeModal(modal) {
     modal.classList.remove('popup_is-opened');
-    document.removeEventListener('keydown', closeModal);
+    document.removeEventListener('keydown', closeByEscape);
 };
 
 
+function closeByEscape(event) {
+    if(event.key === 'Escape') {
+       closeModal(document.querySelector('.popup_is-opened'));
+    }
+}
